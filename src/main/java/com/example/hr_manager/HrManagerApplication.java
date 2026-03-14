0c1e3e72f67b6bf16cdfcd8c4b61ae70aa46df7b
Michał <wmichal133@gmail.com>
2026-03-14 18:23:57 +0100

    Add Spring Boot boilerplate

---
+++ src/main/java/com/example/hr_manager/HrManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagerApplication.class, args);
+	}
+
+}
