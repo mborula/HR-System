f00b908e86259664b71c54fa16c5527394d0c602
Borula Mateusz <bm57939>
2026-03-30 10:52:23 +0200

    static frontend

---
+++ backend/src/main/java/com/example/hr_manager/controller/FrontendController.java
@@ -0,0 +1,13 @@
+package com.example.hr_manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
